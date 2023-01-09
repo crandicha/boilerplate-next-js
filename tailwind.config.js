69102de6e7ce31789c968b499a21cc322e300d81
Charles Randicha <charles.randicha@gmail.com>
2023-01-09 14:40:18 +0700

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ['./pages/**/*.tsx', './components/**/*.tsx', './app/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
